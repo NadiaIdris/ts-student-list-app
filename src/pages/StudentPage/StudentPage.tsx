600de4a373f2af52898e0e7e6f85f50a120e0bf9
Nadia Idris <nadia@fasterlap.com>
2024-05-28 19:22:57 -0500

    Make student page responsive

--- src/pages/StudentPage/StudentPage.tsx
+++ src/pages/StudentPage/StudentPage.tsx
@@ -1,11 +1,12 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { Heading1 } from "../../components/text/Heading1";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { axiosInstance } from "../../api/axiosConfig";
 import { STUDENTS_ENDPOINT } from "../../api/apiConstants";
-import { RiCloseLargeLine } from "react-icons/ri";
+import { CgClose } from "react-icons/cg";
 import { useStudentUid } from "../StudentsPage/StudentsPage";
+import { Button } from "../../components/buttons/Button";
 
 interface IStudent {
   firstName: string;
@@ -22,7 +23,6 @@ interface IStudentFetchData {
 
 // TODO: fix data fetching
 export async function loader({ params }: { params: any }) {
-
   try {
     const response = await axiosInstance.get(
       STUDENTS_ENDPOINT + `/${params.studentId}`
@@ -68,31 +68,54 @@ const StyledStudentOverlay = styled.div`
   top: 0;
   right: 0;
   z-index: 100;
-  width: 500px;
+  max-width: 500px;
+  width: 100%;
   height: 100%;
   background-color: var(--color-white);
+`;
+
+const StyledHeading1 = styled(Heading1)`
+padding: 0 40px;
+
+@media (max-width: 500px) {
+  padding: 0 20px;
+}}
 `;
 
 const StyledStudentDataWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding: 40px;
+  padding: 0 40px;
+
+  @media (max-width: 500px) {
+    padding: 0 20px;
+  }
 `;
 
 const StyledStudentDataRow = styled.div`
   display: flex;
   align-items: center;
+
+  @media (max-width: 400px) {
+    flex-direction: column;
+    align-items: stretch;
+  }
 `;
 
 const StyledDataKey = styled.div`
-  width: 120px;
+  min-width: 120px;
   background-color: red;
 `;
 
-const StyledDataValue = styled.input`
+const StyledDataValue = styled.div`
   flex: 1;
   background-color: lightblue;
+`;
+
+const StyledButtonsWrapper = styled.div`
+  display: flex;
+  gap: 12px;
 `;
 
 const defaultStudentData = {
@@ -109,7 +132,7 @@ const StudentPage = () => {
   const { studentId } = useParams();
   const loaderData: IStudentFetchData | undefined =
     useLoaderData() as IStudentFetchData;
-  const { studentUid, setStudentUid } = useStudentUid();
+  const { setStudentUid } = useStudentUid();
 
   useEffect(() => {
     console.log("studentId from student page: ", studentId);
@@ -120,71 +143,50 @@ const StudentPage = () => {
     console.log("Close student panel");
   };
 
-  const handleStudentDataUpdate = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log("Update student data");
-  };
-
-  // Fetch student data
-  // useEffect(() => {
-  //   const fetchStudent = async () => {
-  //     try {
-  //       const response = await axiosInstance.get(
-  //         STUDENTS_ENDPOINT + `/${studentId}`
-  //       );
-  //       if (response.status === 200) {
-  //         // Rename the keys that have underscores to match the student data object
-  //         const studentData = response.data;
-  //         studentData.firstName = studentData.first_name;
-  //         studentData.lastName = studentData.last_name;
-  //         studentData.dateOfBirth = studentData.date_of_birth;
-  //         setStudentData(studentData);
-  //       }
-  //     } catch (error: any) {
-  //       console.error(error.message);
-  //     }
-  //   };
-  //   fetchStudent();
-  // }, [studentId]);
-
   return (
-    <div>
+    <>
       <StyledCloseIcon onClick={handleCloseStudentPanel}>
-        <RiCloseLargeLine />
+        <CgClose style={{ width: "18px", height: "18px" }} />
       </StyledCloseIcon>
       <StyledStudentOverlay>
-        <Heading1 style={{ padding: "0 40px" }}>
+        <StyledHeading1>
           {loaderData?.studentData.firstName} {loaderData?.studentData.lastName}
-        </Heading1>
+        </StyledHeading1>
         <StyledStudentDataWrapper>
           <StyledStudentDataRow>
             <StyledDataKey>First name: </StyledDataKey>
-            <StyledDataValue
-              value={loaderData?.studentData.firstName}
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                handleStudentDataUpdate(event)
-              }
-            />
+            <StyledDataValue>
+              {loaderData?.studentData?.firstName}
+            </StyledDataValue>
           </StyledStudentDataRow>
           <StyledStudentDataRow>
             <StyledDataKey>Last name: </StyledDataKey>
-            <StyledDataValue value={loaderData?.studentData.lastName} ></StyledDataValue>
+            <StyledDataValue>
+              {loaderData?.studentData?.lastName}
+            </StyledDataValue>
           </StyledStudentDataRow>
           <StyledStudentDataRow>
             <StyledDataKey>Email: </StyledDataKey>
-            {/* <StyledDataValue>{loaderData?.studentData.email}</StyledDataValue> */}
+            <StyledDataValue>{loaderData?.studentData?.email}</StyledDataValue>
           </StyledStudentDataRow>
           <StyledStudentDataRow>
             <StyledDataKey>Gender: </StyledDataKey>
-            {/* <StyledDataValue>{loaderData?.studentData.gender}</StyledDataValue> */}
+            <StyledDataValue>{loaderData?.studentData?.gender}</StyledDataValue>
           </StyledStudentDataRow>
           <StyledStudentDataRow>
             <StyledDataKey>Birthday: </StyledDataKey>
-            {/* <StyledDataValue>{loaderData?.studentData.dateOfBirth}</StyledDataValue> */}
+            <StyledDataValue>
+              {loaderData?.studentData?.dateOfBirth}
+            </StyledDataValue>
           </StyledStudentDataRow>
+          <StyledButtonsWrapper>
+            <Button>Edit</Button>
+            <Button appearance="warning">Delete</Button>
+          </StyledButtonsWrapper>
         </StyledStudentDataWrapper>
       </StyledStudentOverlay>
       <StyledStudentPageCover />
-    </div>
+    </>
   );
 };
 
